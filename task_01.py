09bb0057ad2cee424c9f30dd3451b2502c57f086
LisaWebDiz <lisaadzhna@gmail.com>
2022-11-13 23:11:20 +0300

    Task_01 added

---
+++ task_01.py
@@ -0,0 +1,6 @@
+# Дана строка
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation' 
+# Делим строку по символам "запятая"
+songs = my_favorite_songs.split(',')
+# Выводим в консоль
+print(songs[0], songs[-1], songs[1], songs[-2])
